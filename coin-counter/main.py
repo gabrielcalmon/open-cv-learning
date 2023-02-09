7f50ce73e26a87152759872c05571d09f2acba17
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2023-02-09 11:49:51 -0300

    add size comparation

--- coin-counter/main.py
+++ coin-counter/main.py
@@ -1,7 +1,12 @@
 import cv2 as cv2
 import cvzone
 import numpy as np
+from cvzone.ColorModule import ColorFinder
 #print (cv2.__version__)
+
+myColorFinder = ColorFinder(False)
+# Custom Orange Color
+hsvVals = {'hmin': 10, 'smin': 55, 'vmin': 215, 'hmax': 42, 'smax': 255, 'vmax': 255}
 
 # filtra a imagem inicial para algo que possa ser processado
 def preProcessing(imgOriginal):
@@ -32,22 +37,41 @@ cv2.resizeWindow("Settings", 640, 240)
 cv2.createTrackbar("Threshould1", "Settings", 240, 255, empty)
 cv2.createTrackbar("Threshould2", "Settings", 150, 255, empty)
 
+# sizeMax = {
+#     "10": 1200
+#     "25": 1850
+#     "50": 1500
+#     "100": 2200 }    #10, 25, 50 e 100
+
 while True:
     sucess, img = cap.read()
     if not sucess:
         break
     
     imgPre = preProcessing(img)
-    imgContours, conFound = cvzone.findContours(img, imgPre, 20, filter=8)
+    imgContours, conFound = cvzone.findContours(img, imgPre, minArea=800, filter=8)
 
+    totalMoney = 0
     if conFound:
         for contour in conFound:
             peri = cv2.arcLength(contour['cnt'], True)
             approx = cv2.approxPolyDP(contour['cnt'], 0.02 * peri, True)
             if len(approx)>5:   # um poligono com mais de 5 lados sera considerado um circulo
                 print(contour['area'])
-
+                area = contour['area']
+                if area <= 1300:
+                    totalMoney += 0.10
+                elif 1300<area<=1600:
+                    totalMoney += 0.50
+                elif 1600<area<=1950:
+                    totalMoney += 0.25
+                elif 1950<area<=2200:
+                    totalMoney += 1
+                
+        #print(f'R$: {totalMoney}')
+    
     imgStack = cvzone.stackImages([img, imgPre, imgContours], 2, 1)
+    cvzone.putTextRect(imgStack, f'R$: {totalMoney}', (50,50))
     cv2.imshow('webcam',imgStack)     # abre uma janela com a imagem
     if cv2.waitKey(1) & 0xFF == ord('q'): # condicao de saida do loop
         break
